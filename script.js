be5474f26c34a7aecd08f9b1461752def4317488
Noxx0430 <118098422+Noxx0430@users.noreply.github.com>
2023-05-04 13:09:55 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 // 要素取得
 const startBtn = document.getElementById("startBtn");
 const resetBtn = document.getElementById("resetBtn");
-const checkbox = document.getElementById("checkbox");
+const setting = document.getElementById("setting");
+const effectCheck = document.getElementById("effectCheck");
+const volumeCheck = document.getElementById("volumeCheck");
 const timer = document.getElementById("timer");
 const targetArea = document.getElementById("targetArea");
+const targetAreaFrame = document.getElementById("targetAreaFrame");
+const counter = document.getElementById("counter");
 const cover = document.getElementById("cover");
 const easterEgg = document.getElementById("easterEgg");
 
@@ -34,28 +38,32 @@ easterEgg.ondblclick = function(){
 resetBtn.onclick = function(){
   easterEgg.style.display = "block";
   startBtn.style.display = "block";
-  checkbox.parentElement.style.display = "block";
+  setting.style.display = "block";
   resetBtn.style.display = "none";
   resetBtn.style.opacity = "0";
   resetBtn.setAttribute("disabled", true);
   timer.style.display = "none";
   timer.textContent = "3";
-  Count = 0;
+  counter.innerHTML = "";
+  Count = 5;
   wave = 1;
 }
 
 // ターゲット生成数のカウント
-Count = 0;
+Count = 5;
 
 // ダーミ生成数の変数
 wave = 1;
 
 // 開始後の動作と最初のターゲット生成
+const audio = new Audio("カウントダウン.mp3");
 function start(){
+  audio.play();
   easterEgg.style.display = "none";
   startBtn.style.display = "none";
-  checkbox.parentElement.style.display = "none";
+  setting.style.display = "none";
   timer.style.display = "block";
+  counter.innerHTML = "Point<br>0";
   setTimeout(function(){
     timer.textContent = "2";
   }, 1000)
@@ -64,6 +72,7 @@ function start(){
   }, 2000)
   setTimeout(function(){
     timer.textContent = "";
+    targetAreaFrame.style.display = "block";
     createTarget();
   }, 3000)
 }
@@ -75,29 +84,7 @@ function createTarget(){
   const width = window.innerWidth - 15;
   
   // ダーミの変数を変更
-  if(Count < 10){
-    wave = 1;
-  }else if(Count < 20){
-    wave = 3;
-  }else if(Count < 30){
-    wave = 5;
-  }else if(Count < 40){
-    wave = 8;
-  }else if(Count < 50){
-    wave = 10;
-  }else if(Count < 60){
-    wave = 12;
-  }else if(Count < 70){
-    wave = 14;
-  }else if(Count < 80){
-    wave = 16;
-  }else if(Count < 90){
-    wave = 18;
-  }else if(Count < 100){
-    wave = 20;
-  }else if(Count > 100){
-    wave = Math.floor(Count / 5);
-  }
+  wave = Math.floor(Count / 5);
 
   // ターゲットとダミーをランダム位置に生成
   for(let i=0; i<wave; i++){
@@ -118,6 +105,7 @@ function createTarget(){
   Count += 1;
 }
 
+
 // ターゲット削除
 document.onmouseover = function(){
   // ホバー中の要素のデータ取得
@@ -125,6 +113,12 @@ document.onmouseover = function(){
   const targetState = Number(event.target.getAttribute("data-target-state"));
   if(targetNum){
     if(targetState == 1){
+      // 効果音準備
+      if(volumeCheck.checked){
+        const audio = new Audio("ピロン.mp3");
+        audio.play();
+      }
+
       // カーソル位置取得 => カバー生成
       const top = event.clientY;
       const left = event.clientX;
@@ -137,25 +131,39 @@ document.onmouseover = function(){
         elem[i].remove();
       }
 
+      // カウンター変更
+      counter.innerHTML = "Point<br>" + (Count - 5);
+
       // 次のターゲット生成
       createTarget();
     }else{
-      // ターゲットとダミーを削除
-      const elem = document.querySelectorAll("#target");
-      for(let i=0; i<elem.length; i++){
-        elem[i].remove();
-      }
-
-      // ゲームオーバー時の動作 => タイトル画面へ
-      timer.textContent = "GAME OVER";
-      resetBtn.style.display = "block";
-      cover.style.left = "-10000px"
-      setTimeout(function(){
-        resetBtn.style.opacity = "1";
-        resetBtn.removeAttribute("disabled");
-      }, 2000)
+      gameOver();
     }
   }
+}
+
+function gameOver(){
+  // 効果音準備
+  if(volumeCheck.checked){
+    const audio = new Audio("ビー.mp3");
+    audio.play();
+  }
+
+  // ターゲットとダミーを削除
+  const elem = document.querySelectorAll("#target");
+  for(let i=0; i<elem.length; i++){
+    elem[i].remove();
+  }
+
+  // ゲームオーバー時の動作 => タイトル画面へ
+  timer.textContent = "GAME OVER";
+  targetAreaFrame.style.display = "none";
+  resetBtn.style.display = "block";
+  cover.style.left = "-10000px"
+  setTimeout(function(){
+    resetBtn.style.opacity = "1";
+    resetBtn.removeAttribute("disabled");
+  }, 2000)
 }
 
 // カーソル位置
@@ -169,10 +177,18 @@ window.onmousemove = function(){
   const left = event.clientX;
   Top = top;
   Left = left;
+  const length = document.querySelectorAll("#target").length;
+  if(length){
+      const height = window.innerHeight;
+      const width = window.innerWidth;
+    if(top < 9||left < 9||top > (height - 9)||left > (width - 9)){
+      gameOver();
+    }
+  }
 }
 window.onload = function(){
   setInterval(function(){
-    if(checkbox.checked){
+    if(effectCheck.checked){
       const count = CCount;
       const div = document.createElement("div");
       div.className = "effect";
